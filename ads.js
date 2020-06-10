b97eb6a8349856332ed238e6394935209d030e6b
next37 <57944754+next37@users.noreply.github.com>
2020-06-10 13:11:23 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,1 +1,1 @@
-"use strict";let e=document.createElement("div");e.id="mdp-deblocker-ads",e.style.display="none",document.body.appendChild(e);
+"use strict";let e=document.createElement("div");e.id="mdp-ngaur-ads",e.style.display="none",document.body.appendChild(e);
